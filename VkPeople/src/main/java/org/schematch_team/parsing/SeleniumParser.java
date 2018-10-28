1592bc7cf32d82360e77ddc9b7a11aca1cf819f7
Vitaliy <vit34-95@mail.ru>
2018-10-28 16:18:17 +0300

    added pages, grouos, friends

--- VkPeople/src/main/java/org/schematch_team/parsing/SeleniumParser.java
+++ VkPeople/src/main/java/org/schematch_team/parsing/SeleniumParser.java
@@ -1,8 +1,12 @@
 package org.schematch_team.parsing;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class SeleniumParser {
 
@@ -20,14 +24,15 @@ public class SeleniumParser {
 
     void parse() {
         driver.get("https://www.vk.com");
-        driver.findElement(By.id("index_email")).sendKeys("phone"); //логин и пароль проставить
-        driver.findElement(By.id("index_pass")).sendKeys("pass");
+        driver.findElement(By.id("index_email")).sendKeys("PHONE"); //логин и пароль проставить
+        driver.findElement(By.id("index_pass")).sendKeys("PASS");
         driver.findElement(By.id("index_login_button")).click();
         driver.get("https://www.vk.com/id513142927");
 
         //Тут перебирайте все айдишники
         for (int i = 513142927; i <= 513142927; i++) {
             try {
+                //GET PROFILE INFO
                 driver.get("https://www.vk.com/id" + i);
                 Thread.sleep(1000);
                 System.out.println(driver.findElement(By.className("page_name")).getText());
@@ -50,20 +55,104 @@ public class SeleniumParser {
 
                 }
                 try {
-
                     System.out.println(driver.findElement(By.className("profile_info_full")).getText());
                 } catch (Exception e) {
 
                 }
 
+                //GET ALL FRIENDS
+                driver.get("https://www.vk.com/friends?id=" + i + "&section=all");
+
+                while (true) {
+                    try {
+                        driver.findElement(By.id("show_more")).click();
+                    } catch (Exception exc) {
+                        break;
+                    }
+                }
+
+
                 try {
-                    System.out.println(driver.findElement(By.id("profile_all_groups")).getText());
+                    List<WebElement> webElements = driver.findElements(By.className("friends_field_title"));
+
+                    for (WebElement we : webElements) {
+                        List<WebElement> refList = we.findElements(By.tagName("a"));
+                        for(WebElement we2 : refList) {
+                            System.out.println(we2.getAttribute("href"));
+                        }
+                    }
+
                 } catch (Exception e) {
 
                 }
+
+
+                driver.get("https://www.vk.com/friends?id=" + i + "&section=all");
+
+                while (true) {
+                    try {
+                        driver.findElement(By.id("show_more")).click();
+                    } catch (Exception exc) {
+                        break;
+                    }
+                }
+
+                //PAGES
+                driver.get("https://m.vk.com/id" + i + "?act=idols");
+                scrollToEnd(driver);
+                try {
+                    List<WebElement> webElements = driver.findElements(By.id("id_search_items"));
+
+                    for (WebElement we : webElements) {
+                        List<WebElement> refList = we.findElements(By.tagName("a"));
+                        for (WebElement we2 : refList) {
+                            System.out.println(we2.getAttribute("href"));
+                        }
+                    }
+
+                } catch (Exception e) {
+
+                }
+
+                //GROUPS
+                driver.get("https://m.vk.com/groups?id=" + i);
+                scrollToEnd(driver);
+                try {
+                    List<WebElement> webElements = driver.findElements(By.id("gr_search_items"));
+
+                    for (WebElement we : webElements) {
+                        List<WebElement> refList = we.findElements(By.tagName("a"));
+                        for (WebElement we2 : refList) {
+                            System.out.println(we2.getAttribute("href"));
+                        }
+                    }
+
+                } catch (Exception e) {
+
+                }
+
             } catch (Exception e) {
 
             }
+        }
+    }
+
+    public void scrollToEnd(WebDriver driver) {
+        try {
+            long lastHeight = (Long) ((JavascriptExecutor) driver).executeScript("return document.body.scrollHeight");
+
+            while (true) {
+                ((JavascriptExecutor) driver).executeScript("window.scrollTo(0, document.body.scrollHeight);");
+                Thread.sleep(100);
+
+                long newHeight = (Long) ((JavascriptExecutor) driver).executeScript("return document.body.scrollHeight");
+                if (newHeight == lastHeight) {
+                    break;
+                }
+                lastHeight = newHeight;
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 }
